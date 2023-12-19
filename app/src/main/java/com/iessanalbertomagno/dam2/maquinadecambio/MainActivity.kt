5ac53041e928add81cfda1ee82533dfdcca5888f
Dilmao <200066@iessanalberto.com>
2023-12-19 08:32:53 +0100

    commit clase

--- app/src/main/java/com/iessanalbertomagno/dam2/maquinadecambio/MainActivity.kt
+++ app/src/main/java/com/iessanalbertomagno/dam2/maquinadecambio/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -32,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -64,7 +65,7 @@ fun DarCambio(mainViewModel: MainViewModel) {
     val billetesUi by mainViewModel.uiState.collectAsState()
     val context = LocalContext.current
 
-    Column( // TODO convertir a LazyColumn
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(20.dp),
@@ -75,14 +76,18 @@ fun DarCambio(mainViewModel: MainViewModel) {
             OutlinedTextField(
                 value = importeStr,
                 onValueChange = { importeStr = it },
-                label = { Text(text = "Importe") }, modifier = Modifier.width(150.dp)
+                label = { Text(text = "Importe") },
+                modifier = Modifier.width(150.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
             Spacer(modifier = Modifier.width(20.dp))
 
             OutlinedTextField(
                 value = pagadoStr,
                 onValueChange = { pagadoStr = it },
-                label = { Text(text = "Pagado") }, modifier = Modifier.width(150.dp)
+                label = { Text(text = "Pagado") },
+                modifier = Modifier.width(150.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
         }
         Spacer(modifier = Modifier.height(20.dp))
@@ -93,9 +98,10 @@ fun DarCambio(mainViewModel: MainViewModel) {
                 mainViewModel.onGiveChange(billetesUi.pagado, billetesUi.importe)
 
                 if (billetesUi.cambio >= 0) {
-                    // TODO comentar el if  para ver por que hay que dar 3 veces al boton
-                    //  Codigo a utilizar: mensaje = "Cambio: ${billetesUi.pagado} - ${billetesUi.importe} = ${billetesUi.cambio}€"
-                    //  (solucion fea: for(i = 0; i <= 3; i++))
+                    // TODO para ver por que hay que dar al botón 3 veces:
+                    //  mensaje = "Cambio: ${billetesUi.pagado} - ${billetesUi.importe} = ${billetesUi.cambio}€"
+                    //  (solucion fea: for(i = 0; i <= 3; i++)) {mensaje...}
+                    //  igualmente la variable mensaje no se deberia de usar al final
                     mensaje = "${billetesUi.cambio}€"
                 } else {
                     mensaje = "Cantidad pagada insuficiente"
@@ -110,28 +116,36 @@ fun DarCambio(mainViewModel: MainViewModel) {
         }) {
             Text(text = "Dar cambio")
         }
+        Spacer(modifier = Modifier.height(20.dp))
 
 
-        // EUROS
-        CrearLineaImagenes(R.drawable._10000, "Billete de 100 euros", 100.dp)
+        // euros
+        CrearLineaImagenes(listOf(
+            Triple(R.drawable._10000, "Billete de 100 euros", 60.dp),
+            Triple(R.drawable._5000, "Billete de 50 euros", 60.dp),
+            Triple(R.drawable._2000, "Billete de 20 euros", 60.dp),
+            Triple(R.drawable._1000, "Billete de 10 euros", 60.dp),
+            Triple(R.drawable._500, "Billete de 5 euros", 60.dp),
+        ))
+        Spacer(modifier = Modifier.height(20.dp))
 
-        CrearLineaImagenes(R.drawable._5000, "Billete de 50 euros", 100.dp)
-        CrearLineaImagenes(R.drawable._2000, "Billete de 20 euros", 100.dp)
-        CrearLineaImagenes(R.drawable._1000, "Billete de 10 euros", 100.dp)
+        // centimos (1ª linea)
+        CrearLineaImagenes(listOf(
+            Triple(R.drawable._200, "Moneda de 2 euros", 50.dp),
+            Triple(R.drawable._100, "Moneda de 1 euro", 50.dp),
+            Triple(R.drawable.__50, "Moneda de 50 cents", 50.dp),
+            Triple(R.drawable.__20, "Moneda de 20 cents", 50.dp),
+        ))
+        Spacer(modifier = Modifier.height(20.dp))
 
-        CrearLineaImagenes(R.drawable._500, "Billete de 5 euros", 100.dp)
-        CrearLineaImagenes(R.drawable._200, "Moneda de 2 euros", 100.dp)
-        CrearLineaImagenes(R.drawable._100, "Moneda de 1 euro", 70.dp)
-        // TODO buscar otra moneda de 1€ (puto asco esta)
-
-        // CENTIMOS
-        CrearLineaImagenes(R.drawable.__50, "Moneda de 50 cents", 40.dp)
-        CrearLineaImagenes(R.drawable.__20, "Moneda de 20 cents", 40.dp)
-        CrearLineaImagenes(R.drawable.__10, "Moneda de 10 cents", 40.dp)
-
-        CrearLineaImagenes(R.drawable.__5, "Moneda de 5 cents", 40.dp)
-        CrearLineaImagenes(R.drawable.__2, "Moneda de 2 cents", 40.dp)
-        CrearLineaImagenes(R.drawable.__1, "Moneda de 1 cent", 40.dp)
+        // centimos (2ª linea)
+        CrearLineaImagenes(images = listOf(
+            Triple(R.drawable.__10, "Moneda de 10 cents", 50.dp),
+            Triple(R.drawable.__5, "Moneda de 5 cents", 50.dp),
+            Triple(R.drawable.__2, "Moneda de 2 cents", 50.dp),
+            Triple(R.drawable.__1, "Moneda de 1 cent", 50.dp),
+        ))
+        Spacer(modifier = Modifier.height(20.dp))
 
         // Cambio TODO (esto no es necesario)
         Text(text = mensaje)
@@ -140,24 +154,23 @@ fun DarCambio(mainViewModel: MainViewModel) {
 
 
 @Composable
-fun CrearLineaImagenes(resourceId: Int, descripcion: String, tamanno: Dp) {
+fun CrearLineaImagenes(images: List<Triple<Int, String, Dp>>) {
     Row(
-        horizontalArrangement = Arrangement.Start,
+        horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.fillMaxWidth()
     ) {
-        // Se separa la imagen del borde y se imprime
-        Spacer(modifier = Modifier.width(10.dp))
-        Image(
-            painter = painterResource(id = resourceId),
-            contentDescription = descripcion,
-            modifier = Modifier.size(tamanno)
-        )
-
-        // Se separa del borde y se imrpime la cantidad de billetes a devolver
-        Spacer(modifier = Modifier.width(20.dp))
-        Text(text = "Hola wenas :D")
+        images.forEach{ (resourceId, description, tamanno) ->
+            Spacer(modifier = Modifier.width(10.dp))
+            Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = resourceId),
+                    contentDescription = description,
+                    modifier = Modifier.size(tamanno)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(text = "0")
+            }
+        }
     }
-    
-    Spacer(modifier = Modifier.height(20.dp))
 }
